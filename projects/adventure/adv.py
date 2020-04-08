1829f9dd44dbe16b3b0c6b2d7a6d9ff6795a25b4
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-04-08 16:12:03 +0800

    Remove unused log messages and clarify variable names

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -4,7 +4,7 @@ from world import World
 
 import random
 from ast import literal_eval
-from util import Stack, Queue, select_exit, reverse_direction, find_unexplored_room
+from util import Stack, Queue, find_unexplored_room
 
 # Load world
 world = World()
@@ -27,11 +27,8 @@ world.print_rooms()
 player = Player(world.starting_room)
 
 # Fill this out with directions to walk
-# traversal_path = ['n', 'n']
+# EXAMPLE: traversal_path = ['n', 'n']
 traversal_path = []
-
-print(f"NUMBER OF ROOMS: {len(room_graph)}")
-print(f"ROOM_GRAPH: {room_graph}")
 
 # Create an exploration graph
 explore_graph = {}
@@ -43,81 +40,47 @@ stack = Stack()
 stack.push(player.current_room)
 
 while len(explore_graph) < len(room_graph):
-    # while stack.size() > 0:
+    # Explore rooms using DFS
     current_room = stack.pop()
     room_exits = player.current_room.get_exits()
 
     # Check if current room is in explore_graph:
-    #print(f"CURRENT ROOM: {current_room.id}")
     if current_room.id not in explore_graph:
         # Add to explore_graph
-
         explore_graph[current_room.id] = {exit: '?' for exit in room_exits}
-        print(
-            f"explore_graph[current_room.id]: {current_room.id}: {explore_graph[current_room.id]}")
 
-        print(f"ROOM EXITS {room_exits}")
-
-    # Randomly select an exit
+    # Travel through unexplored exits
     possible_exits = [
         direction for (direction, room_id) in explore_graph[current_room.id].items() if room_id == '?']
 
-    # if len(explore_graph[current_room.id].keys()) == 1 and len(possible_exits) > 0:
-    #     direction = player.current_room.get_exits()[0]
-    #     print(f"EXITS: {direction}")
-    #     traversal_path.append(direction)
-    #     prev_room = current_room
-    #     player.travel(direction)
-    #     stack.push(player.current_room)
-
     if len(possible_exits) > 0:
-        print(f"POSSIBLE EXITS: {possible_exits}")
+        # Randomly select an exit
         random_exit = random.choice(possible_exits)
-        #print(f"random_exit: {random_exit}")
+        # Map out traversal path
         traversal_path.append(random_exit)
+        # Travel through and map out room exits
         prev_room = current_room
         player.travel(random_exit)
         explore_graph[prev_room.id][random_exit] = player.current_room.id
+        # Add the next room to the stack
         stack.push(player.current_room)
 
     else:
-        print(f"graph before BFS: {explore_graph}")
-        print(f"starting room before BFS: {player.current_room.id}")
-        find_exit = find_unexplored_room(player, explore_graph)
-        print(f"UNEXPLORED PATH: {find_exit}")
+        # Find shortest path to unexplored exits
+        find_exit_path = find_unexplored_room(player, explore_graph)
 
-        # if len(find_exit) == 1:
-        #     find_exit = find_unexplored_room(player, explore_graph)
-
-        if find_exit:
-            f = open("find_exit.txt", "a+")
-            f.write(str(find_exit))
-            f.close()
-            for step in find_exit:
-                print(f"starting in room {player.current_room.id}")
-                print(f"GOING {step}")
+        # Append unexplored exits shortest path to main traversal path
+        if find_exit_path:
+            for step in find_exit_path:
                 traversal_path.append(step)
                 player.travel(step)
-                print(f"In room {player.current_room.id}")
-                print(f"PREVIOUS ROOM {player.prev_room.id}")
 
-                if player.current_room.id in explore_graph:
-                    explore_graph[player.prev_room.id][step] = player.current_room.id
-
-                else:
-                    print(
-                        f"ADD ROOM {player.current_room.id} TO STACK")
+                if player.current_room.id not in explore_graph:
                     stack.push(player.current_room)
                     break
 
-            print(f"ending room {player.current_room.id}")
-            print(f"graph after BFS: {explore_graph}")
-
-    print(f"DEAD END AT ROOM {player.current_room.id} KEEP IT GOING")
+    # Handle dead ends
     stack.push(player.current_room)
-    print(
-    f"Number of rooms in explore_graph: {len(explore_graph)}")
-    # print(f"current stack {stack.stack}")
 
 
 
--- projects/adventure/util.py
+++ projects/adventure/util.py
@@ -1,8 +1,5 @@
 import random
 
-# Note: This Queue class is sub-optimal. Why?
-
-
 class Queue():
     def __init__(self):
         self.queue = []
@@ -37,16 +34,6 @@ class Stack():
         return len(self.stack)
 
 
-def select_exit(exits):
-    possible_exits = [k for (k, v) in exits.items() if v == '?']
-
-    try:
-        random_exit = random.choice(possible_exits)
-    except IndexError:
-        return None
-    return random_exit
-
-
 def reverse_direction(direction):
     directions = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
 
@@ -58,49 +45,34 @@ def reverse_direction(direction):
 
 
 def find_unexplored_room(player, graph):
+    """Find shortest path to unexplored rooms"""
+
     queue = Queue()
     visited_rooms = set()
     starting_room_id = player.current_room.id
-    print(
-        f"STARTING CURRENT PATH: {[(None, starting_room_id)]}")
     queue.enqueue([(None, starting_room_id)])
 
     while queue.size() > 0:
         current_path = queue.dequeue()
-        last_room_id = current_path[-1][1]
+        current_room_id = current_path[-1][1]
 
-        if '?' in graph[last_room_id].values():
-            # path =
-            print(
-                f"FOUND SOMETHING in room {last_room_id}: {graph[last_room_id]}")
-            print(f"current_path: {current_path}")
+        if '?' in graph[current_room_id].values():
+            # Create and return directions
             unexplored_exit = [
-                k for (k, v) in graph[last_room_id].items() if v == '?']
+                direction for (direction, room_id) in graph[current_room_id].items() if room_id == '?']
             unexplored_path = [direction for (
                 direction, room_id) in current_path if direction is not None]
             unexplored_path.extend(unexplored_exit)
+
             return unexplored_path
-            # return unexplored_exit
 
-        #print(f"NUMBER OF EXITS: {len(graph[last_room_id].items())}")
-        # if len(graph[last_room_id].items()) == 1:
-        #     print(f"NUMBER OF EXITS: {len(graph[last_room_id].items())}")
-        #     return None
-        #     print(f"DEAD END in ROOM {last_room_id}")
-        #     backtrack_path = player.current_room.get_exits()
-        #     print(f"BACKTRACK_PATH: {backtrack_path}")
-            
-        #     return backtrack_path
-
-        # else: 
-        if last_room_id not in visited_rooms:
-            visited_rooms.add(last_room_id)
-            for (direction, room_id) in graph[last_room_id].items():
+        if current_room_id not in visited_rooms:
+            visited_rooms.add(current_room_id)
+            for (direction, room_id) in graph[current_room_id].items():
                 pair = (direction, room_id)
-
                 path_copy = list(current_path)
                 path_copy.append(pair)
                 queue.enqueue(path_copy)
 
-    print(f"NO JOY CURRENT PATH: {current_path}")
+    print(f"No current path: {current_path}")
     return None
